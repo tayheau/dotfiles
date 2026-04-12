bad43f37dbd98451f0debbd6ff4ae70562dab57b
Théo H. <thelava12@gmail.com>
2026-04-12 14:05:43 +0200

    up typst env

--- .bash_profile
+++ .bash_profile
@@ -12,6 +12,8 @@ HISTFILESIZE=2000
 
 PS1='$(err=$?; [[ $err -ne 0 ]] && printf "\[\e[41m\e[1m\]";echo "\w\[\e[0m\]";)$(git rev-parse --abbrev-ref HEAD 2>/dev/null | sed "s@\(.*\)@ (\1)@")$ '
 
+TYPST_PACKAGE_PATH=$(typst info 2>&1 | awk -F'  +' '/^  Package path/{print $NF}')
+
 # if [ -n "$BASH_VERSION" ]; then
 #     # include .bashrc if it exists
 #     if [ -f "$HOME/.bashrc" ]; then
--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@ done
 
 # [[ command -v nvim  >/dev/null 2>&1 ]] && echo "lol"
 
-mkdir -p ~/.local/share/typst/packages/local/tayheau-slides/1.0.0
-ln -s "$(pwd)/tayheau_slides.typ" ~/.local/share/typst/packages/local/tayheau-slides/1.0.0/lib.typ
-cat > ~/.local/share/typst/packages/local/tayheau-slides/1.0.0/typst.toml <<'EOF'
+
+mkdir -p "${TYPST_PACKAGE_PATH}/local/tayheau-slides/1.0.0"
+ln -s "$(pwd)/tayheau_slides.typ" "${TYPST_PACKAGE_PATH}/local/tayheau-slides/1.0.0/lib.typ"
+cat > "${TYPST_PACKAGE_PATH}/local/tayheau-slides/1.0.0/typst.toml" <<'EOF'
 [package]
 name = "tayheau-slides"
 version = "1.0.0"
