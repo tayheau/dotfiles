1a20cc26d0c1b3aee25dbe0623a57fce5044bd8e
tayheau <thopsore@pasteur.fr>
2026-07-22 14:54:25 +0200

    sym and HOME expand

--- projectselect.sh
+++ projectselect.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-start_dir="~/code/$(ls -d -l ~/code/* | sed "s@^.*/@@" | fzf)"
+start_dir="$HOME/code/$(find -L ~/code -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | fzf)"
 session_name=$(basename "$start_dir")
 if ! tmux has-session -t "$session_name" 2>/dev/null; then
 	tmux new -s "$session_name" -c "$start_dir" -d
+	cd "$start_dir"
 fi
 tmux switch-client -t "$session_name"
